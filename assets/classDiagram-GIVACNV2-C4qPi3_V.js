3ce5ff028933bf5343bc9da780e1d9b522773dfc
Felipe Renó <flp1988@gmail.com>
2025-06-01 13:06:41 +0100

    Deploy new version of the site

---
+++ assets/classDiagram-GIVACNV2-C4qPi3_V.js
@@ -0,0 +1,1 @@
+import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-c3y5zsgk.js";import{_ as l}from"./index-CaMUEiG-.js";import"./chunk-RZ5BOZE2-BaSgjV7R.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
